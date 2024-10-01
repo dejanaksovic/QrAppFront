10199de43e3fa595d66526c6ce57ba4e68388bc9
Effortless <imdejan00@gmail.com>
2024-10-01 15:01:58 +0200

    Finished implementing popup

--- assets/Popup.js
+++ assets/Popup.js
@@ -1,26 +1,27 @@
 export class Popup {
-  constructor(message, iconPath) {
-    this.message = message;
-    this.iconPath = iconPath;
+  constructor() {
     // Form main container
     const mainContainer = document.createElement("div");
-    mainContainer.classList.add("enable-sticky");
+    mainContainer.classList.add("enable-sticky", "hidden");
+    this.mainContainer = mainContainer;
     // Sticky container
     const stickyContainer = document.createElement("div");
     stickyContainer.classList.add("popup-container", "bg-accent");
     // Svg for popu
     const svg = document.createElement("img");
-    svg.src = iconPath;
+    this.svgElement = svg;
     // Message paragraph
     const messageElement = document.createElement("p");
-    messageElement.textContent = message;
+    this.messageElement = messageElement;
     // button div
     const buttonsContainer = document.createElement("div");
     buttonsContainer.classList.add("buttons-container");
     // Make buttons
     const confirmButton = document.createElement("button");
     confirmButton.textContent = "POTVRDI";
+    this.confirmButton = confirmButton;
     const rejectButton = document.createElement("button");
+    this.rejectButton = rejectButton;
     rejectButton.textContent = "ODUSTANI";
     // Place into containers
     buttonsContainer.appendChild(confirmButton);
@@ -34,4 +35,16 @@ export class Popup {
     document.body.prepend(mainContainer);
   }
 
+  showPopup(message, path, handler) {
+    this.confirmButton.addEventListener("click", (e) => {
+      handler(e);
+      this.mainContainer.classList.add("hidden");
+    }, {once: true});
+    this.messageElement.textContent = message;
+    this.svgElement.src = path;
+    this.rejectButton.addEventListener("click", () => {
+      this.mainContainer.classList.add("hidden");
+    }, {once: true});
+    this.mainContainer.classList.remove("hidden");
+  } 
 }
--- popoup/app.js
+++ popoup/app.js
@@ -1,3 +1,6 @@
 import { Popup } from "../assets/Popup.js";
 
-const popup = new Popup("Testiramo za korisnika", "../svgs/del-user.svg");
+const popup = new Popup();
+popup.showPopup("Please work man", "../svgs/del-user.svg", () => {
+  console.log("This shit actually fucking works");
+})
