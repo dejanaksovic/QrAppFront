76c8d0cdb10eec47eb7612b4bed5c7f29bbd1564
DekkyVideoplus <dekky@videoplus.com>
2024-10-02 14:20:16 +0200

    Implemented fetching users and error handling, redirecting on invalid password aswell

--- admin/view-user-all/app.js
+++ admin/view-user-all/app.js
@@ -1,5 +1,88 @@
-import { Popup } from "../../assets/Popup.js";
-const popup=new Popup();
-popup.showPopup("Da li hocete da prihvatite novog korisnika?","../../svgs/del-user.svg",()=>{
-  console.log("Radi");
-})
+import { FlashMessage } from "../../assets/Flash";
+import { getBasePath, URL } from "../../assets/helpers";
+import { PageShifter } from "../../assets/Pageshifter";
+// Elements
+const usersContainer = document.getElementById("users-container");
+
+// pages-setup
+const pages = ["main-page", "500"];
+const pageShifter = new PageShifter(pages, "main-page");
+
+// Flash message
+const flashMessage = new FlashMessage();
+// Assets
+const addUser = (user) => {
+  // user container
+  const userContainer = document.createElement("div");
+  userContainer.classList.add("user");
+  // icon
+  const icon = document.createElement("img");
+  icon.src = "../../svgs/user-pfp.svg";
+  userContainer.appendChild(icon);
+  // Name and balance
+  const nameAndCoinsContainer = document.createElement("div");
+  nameAndCoinsContainer.classList.add("name-and-coins");
+  const nameContainer = document.createElement("p");
+  nameContainer.id = "name";
+  nameContainer.textContent = user.Name;
+  const coinsContainer = document.createElement("p");
+  coinsContainer.id = "coins";
+  coinsContainer.textContent = user.Coins;
+  nameAndCoinsContainer.appendChild(nameContainer);
+  nameAndCoinsContainer.appendChild(coinsContainer);
+  userContainer.appendChild(nameAndCoinsContainer);
+  // Buttons
+  const delButton = document.createElement("button");
+  delButton.classList.add("remove-button");
+  delButton.textContent = "OBRISI";
+  const changeButton = document.createElement("button");
+  changeButton.classList.add("manage-button");
+  changeButton.textContent = "IZMENI";
+  userContainer.appendChild(delButton);
+  userContainer.appendChild(changeButton);
+  // Add to main container
+  usersContainer.appendChild(userContainer);
+}
+
+const handleGetUsers = async () => {
+  let res;
+  let data; 
+  try {
+    res = await fetch(`${URL}/users`, {
+      headers: {
+        "Content-Type" : "application/json",
+        "authorization": "Hx",
+      }
+    });
+    data = await res.json();
+    console.log(data);
+  }
+  catch(err) {
+    pageShifter.showPageOnly("500");
+  }
+
+  const { users, message } = data;
+
+  // Handle erros
+  if(res.status === 401 || res.status === 403) {
+    return window.location.assign(`${getBasePath()}/admin/login/index.html`);
+  }
+
+  if(res.status === 500) {
+    return pageShifter.showPageOnly("500");
+  }
+
+  if(message) {
+    return flashMessage.showMessage(message, "error");
+  }
+  
+  if(res.ok) {
+    // If success populate users
+    for (let user of users) {
+      addUser(user);
+    }
+    return;
+}
+}
+
+window.onload = handleGetUsers();
