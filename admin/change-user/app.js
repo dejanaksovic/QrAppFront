13afdbc1c517ce1a86d80e86f8ae696820a90fa2
DekkyVideoplus <dekky@videoplus.com>
2024-10-02 16:01:25 +0200

    Started implementing logic for user change

---
+++ admin/change-user/app.js
@@ -0,0 +1,74 @@
+import { getBasePath, getUserIdFromUrl, URL } from "../../assets/helpers";
+import { PageShifter } from "../../assets/Pageshifter";
+import { Router } from "../../assets/PagePaths";
+
+// ELEMENTS
+const nameInput = document.getElementById("name-input");
+const balanceInput = document.getElementById("balance-input");
+
+const confirmButton = document.getElementById("confirm-button");
+const cancelButton = document.getElementById("cancel-button");
+
+// Pages setup
+const pages = ["main-container", "404", "500"]
+const pageShifter = new PageShifter(pages, "main-container");
+
+// HANDLERS 
+const handleCancel = () => {
+  window.location.assign(`${getBasePath()}/admin/view-user-all/index.html`);
+}
+const handleChange = async () => {
+  let res;
+  let data;
+  try {
+    res = await fetch(`${URL}/users/${id}`, {
+      headers: {
+        "Content-Type" : "application/json",
+        "authorization" : `${adminPassword}`,
+      },
+      body: JSON.stringify({
+        name: nameInput.value,
+        balance: balanceInput.value
+      })
+    });
+    data = await res.json();
+  }
+  catch(err) {
+    return pageShifter.showPageOnly("500");
+  }
+
+  const { user, message } = data;
+
+  if(res.ok) {
+    return window.location.assign(Router.adminViewAll);
+  }
+
+  if(res.status === 401 || res.status === 403) {
+    sessionStorage.removeItem("adminPassword");
+    localStorage.removeItem("adminPassword");
+    return window.location.assign(Router.adminLogin);
+  }
+
+  if(res.status === 404) {
+    return pageShifter.showPageOnly("404");
+  }
+
+  if(res.status === 500) {
+    return pageShifter.showPageOnly("500");
+  }
+}
+
+// ASSIGN HANDLERS
+cancelButton.addEventListener("click", handleCancel);
+
+// DEFAULT BEHAVIOUR
+const id = getUserIdFromUrl(window.location.search);
+if(!id) {
+  pageShifter.showPageOnly("404");
+  throw Error("User not found");
+}
+
+const adminPassword = sessionStorage.getItem("adminPassword");
+if(!adminPassword) {
+  window.location.assign(Router.adminLogin);
+}
