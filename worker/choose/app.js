7098155322d955577535a801eaae52354819f26f
Effortless <imdejan00@gmail.com>
2024-10-20 00:54:27 +0200

    Routing for worker done

---
+++ worker/choose/app.js
@@ -0,0 +1,24 @@
+import { getUserIdFromUrl } from "../../assets/helpers";
+import { Router } from "../../assets/PagePaths";
+import { PageShifter } from "../../assets/Pageshifter";
+
+const [chargeButton, addButton] = document.querySelectorAll("button");
+
+// pages
+const pages = ["main", "404"]
+const shifter = new PageShifter(pages, "main");
+
+// assets
+const id = getUserIdFromUrl(window.location.search);
+
+// handlers
+chargeButton.addEventListener("click", () => {
+  Router.workerCharge(id);
+})
+
+// Defaults
+if(!id) {
+  shifter.showPageOnly("404");
+  throw Error("User not found");
+}
+
