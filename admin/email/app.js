3170995e74f66f9449b30ab407cb5cdb6b473468
DekkyVideoplus <dekky@videoplus.com>
2024-11-15 16:15:15 +0100

    Front ui for sending mail to all users done

---
+++ admin/email/app.js
@@ -0,0 +1,50 @@
+import { URL } from "../../assets/helpers";
+import { Router } from "../../assets/PagePaths";
+import { PageShifter } from "../../assets/Pageshifter";
+import { RequestHandler } from "../../assets/RequestHandler";
+
+// Elements
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+const [confirmButton, cancelButton] = document.querySelectorAll(".action button");
+
+// Pages setup
+const pages = ["main", "500"];
+const shifter = new PageShifter(pages, "main");
+
+// Handler
+const handler = new RequestHandler(shifter, null, "admin");
+// Helpers
+const adminPassword = sessionStorage.getItem("adminPassword");
+
+// Handlers
+const handleSendMailAll = async () => {
+  console.log(subject.value);
+  console.log(message.value);
+  console.log(`${URL}/communication/email/all`);
+  const options = {
+    method: "POST",
+    url: `${URL}/communication/email/all`,
+    password: adminPassword,
+    body: {
+      subject: subject.value,
+      message: message.value,
+    }
+  }
+
+  const res = await handler.doRequest(options, "Uspešno poslat mail");
+  return;
+}
+const handleCancel = () => {
+  Router.adminViewAllArticles();  
+}
+
+// Connect handlers
+confirmButton.addEventListener("click", handleSendMailAll);
+cancelButton.addEventListener("click", handleCancel);
+
+// Default
+if(!adminPassword) {
+  Router.adminLogin();
+}
